0d23253c7896e6b168924a39bb8784d74bb0ca09
Joel Barciauskas <barciajo@gmail.com>
2018-11-08 15:35:35 -0500

    Example code for gtfs

--- service.py
+++ service.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import os
 
+import requests
 from datadog import api, initialize, ThreadStats
+from google.transit import gtfs_realtime_pb2
 
 options = {
     'api_key': os.environ.get('DD_API_KEY'),
@@ -12,6 +14,21 @@ initialize(**options)
 
 
 def handler(event, context):
+    tripFeed = gtfs_realtime_pb2.FeedMessage()
+    tripResponse = requests.get('https://cdn.mbta.com/realtime/TripUpdates.pb')
+    tripFeed.ParseFromString(tripResponse.content)
+    for entity in tripFeed.entity:
+        if entity.HasField('trip_update'):
+            pass
+#            print(entity.trip_update)
+
+    saFeed = gtfs_realtime_pb2.FeedMessage()
+    saResponse = requests.get('https://cdn.mbta.com/realtime/Alerts.pb')
+    saFeed.ParseFromString(saResponse.content)
+    for entity in saFeed.entity:
+        if entity.HasField('alert'):
+            print(entity.alert)
+
     stats = ThreadStats()
     stats.start()
     stats.increment('mbta.api.test.counter')
@@ -22,4 +39,3 @@ def handler(event, context):
     tags = ['version:1', 'application:web']
 
     api.Event.create(title=title, text=text, tags=tags)
-
